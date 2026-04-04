77cde02d25f7556cdeb2c2a743924617b7acec0a
Orucc <orucs703@gmail.com>
2026-04-04 19:05:31 +0000

    fds

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,100):
-	if i < 10:
-		print(f"0{i}",end=", ")
-	else:
-		print(i,end=", ")
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
